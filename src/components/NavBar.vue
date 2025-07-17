9d1821629b33341738e492f246612aafe24cd97f
rahman <rahmannurhayadi@gmail.com>
2025-07-17 10:58:21 +0700

    change navbar layout

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,46 +1,15 @@
 <template>
-    <nav class="z-50 h-fit fixed inset-0 bg-white/50 backdrop-blur-sm">
-        <div class="flex flex-row justify-between items-center p-4">
-            <!-- Logo -->
-            <div class="z-1 w-full" id="logo">
-                <h1 class="text-3xl font-black">RHMN</h1>
-            </div>
-            <button
-                class="relative flex flex-col justify-center items-center w-10 h-10 z-1 cursor-pointer lg:hidden"
-                @click="showNav">
-                <!-- Garis 1 -->
-                <span class="absolute w-full h-2 rounded transition-all duration-300 transform"
-                    :class="isNav ? 'rotate-45 translate-y-0 bg-light-gray' : '-translate-y-2.5 bg-primary'"></span>
-                 <!-- Garis 2 -->
-                <span class="w-full h-1 rounded transition-all duration-300"
-                    :class="isNav ? 'scale-0' : 'scale-100 bg-primary'"></span>
-                 <!-- Garis 3 -->
-                <span class="absolute w-full h-2 rounded transition-all duration-300 transform"
-                    :class="isNav ? '-rotate-45 translate-y-0 bg-light-gray' : 'translate-y-2.5 bg-primary'"></span>
-            </button>
-             <div class="fixed bg-primary w-full h-screen inset-0 box-border px-4 flex flex-col pt-10 lg:static lg:bg-transparent lg:h-full lg:p-0"
-                v-show="isNav">
-                <ul class="flex flex-col gap-y-8 justify-center h-full lg:flex-row lg:gap-x-2">
-                    <li v-for="(menu, i) in menus"
-                        class="text-3xl py-2 group cursor-pointer flex flex-row overflow-hidden border-b lg:text-lg lg:text-center lg:border-b-0 text-focus-in text-light-gray lg:text-primary"
-                        :style="{ animationDelay: animationDelay(i) }"
-                        :class="i == 4 ? 'lg:border-l lg:border-r' : 'lg:border-l'" 
-                        @click="jumpToSection(menu.path)">
-                        <span
-                            class="inline-block shrink-0 w-full -translate-x-full group-hover:translate-x-0 opacity-0 group-hover:opacity-100 transition-all duration-500 lg:min-w-18">{{ menu.name }}</span>
-                        <span
-                            class="inline-block shrink-0 w-full -translate-x-full group-hover:translate-x-0 group-hover:opacity-0 transition-all duration-500 lg:min-w-18">{{ menu.name }}</span>
-                    </li>
-                </ul>
-                <Contact class="lg:hidden text-light-gray"/>
-            </div>
-        </div>
+    <nav class="z-50 left-1/2 -translate-x-1/2 fixed shadow-sm p-3 top-2 max-lg:top-4/5 rounded-full bg-white/50 backdrop-blur-lg w-full overflow-scroll scroll-hidden max-md:w-[80%] lg:w-max max-lg:w-4/5 max-sm:w-80">
+        <ul class="inline-flex w-full gap-x-4">
+            <li v-for="menu in menus" class="w-28 rounded-full shrink-0 text-center p-2 hover:bg-primary hover:text-soft-ivory transition-colors duration-300 cursor-pointer" @click="jumpToSection(menu.path)">
+                {{ menu.name }}
+            </li>
+        </ul>
     </nav>
 </template>
 
 <script setup>
 import { onMounted, onUnmounted, ref } from 'vue'
-import Contact from './util/Contact.vue'
 import { SplitText } from 'gsap/SplitText'
 import gsap from 'gsap'
 
@@ -107,3 +76,10 @@ onUnmounted(() => {
     window.removeEventListener('resize', updateNavVisibility)
 })
 </script>
+
+
+<style scoped>
+.scroll-hidden { 
+    scrollbar-width: none;
+}
+</style>
